0a15eef3e214c90cde6e4643f69842602be9033b
purpeel <149309753+purpeel@users.noreply.github.com>
2025-06-18 22:58:40 +0300

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/glebr/prog/kaf22/sem2/lab3
-# Build directory: /home/glebr/prog/kaf22/sem2/lab3/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
